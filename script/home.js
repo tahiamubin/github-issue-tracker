cc26b4d35e332410ad14e96e29e730c5d51c69d8
tahiamubin <tahia.mubin1@gmail.com>
2026-03-08 00:48:23 +0600

    border added

--- script/home.js
+++ script/home.js
@@ -45,10 +45,20 @@ const priorityColor = {
 
 };
 
+const  cardBorder = (status) => {
+  if (status === "open"){
+    return "border-t-[7px] border-solid border-[#00A96E]";
+  }
+  else if (status === "closed"){
+    return "border-t-[7px] border-solid border-[#A855F7]"
+  }
+
+}
+
 
 
 function displayIssueButton (cards){
-    const issueCardContainer = document.getElementById("issueCards");
+   const issueCardContainer = document.getElementById("issueCards");
     console.log(issueCardContainer)
     issueCardContainer.innerHTML = "";
     for (let card of cards){
@@ -57,13 +67,15 @@ function displayIssueButton (cards){
        const issueCard = document.createElement("div");
         issueCard.innerHTML = `
         <!-- cards -->
-        <div id="issueCards" class="card space-y-4 bg-white p-4 shadow-xl">
+      <div id="issueCards" >
+          <!-- cards -->
+             <div id="eachCard"  class="card space-y-4 bg-white p-4 shadow-xl ${cardBorder(card.status)}">
           <!-- top -->
-          <div class=" flex justify-between p-4 ">
+              <div  class=" flex justify-between p-4 ">
             <button class="${priorityColor[card.priority]} py-2 px-8 rounded-lg ">${card.priority}</button>
-          </div>
+              </div>
            <!-- middle -->
-          <div class="space-y-4">
+             <div class="space-y-4">
             <h1 class="text-xl font-bold text-[#1F2937]">${card.title}</h1>
             <p class=" text-[#1F2937] line-clamp-2">${card.description}</p>
              <div class="inner-butt">
@@ -72,11 +84,12 @@ function displayIssueButton (cards){
              </div>
           </div>
            <!-- end -->
-            <div class="text-xl text-[#1F2937] space-y-2">
+               <div class="text-xl text-[#1F2937] space-y-2">
               <p><span>#${card.id}</span> by <span>${card.assignee ? card.assignee : 'No name found' }</span></p>
               <p>${new Date(card.createdAt).getMonth()+1}/${new Date(card.createdAt).getDate()}/${new Date(card.createdAt).getFullYear()}</p>
-            </div>
+              </div>
         </div>
+      </div>
         `
        issueCardContainer.append(issueCard) ;
     }
@@ -89,6 +102,8 @@ function showOpenCard (button) {
   const openCard = allCard.filter(data => data.status =="open");
    removeActive();
   button.classList.add("active");
+  cardBorder();
+  
   displayIssueButton(openCard);
  
 }
@@ -97,6 +112,7 @@ function showClosedCard(button){
   const closeCard = allCard.filter (data => data.status === "closed");
   removeActive();
   button.classList.add("active");
+  cardBorder();
   displayIssueButton(closeCard);
  
 
